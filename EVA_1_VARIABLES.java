edf60bcdd001746948b135ab4708eed5c5378420
Alexisdbe <x55rh2f4mc@privaterelay.appleid.com>
2026-01-29 17:46:04 -0600

    Add files via upload

---
+++ EVA_1_VARIABLES.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_variables;
+
+/**
+ *
+ * @author burci
+ */
+public class EVA_1_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;
+        char caracter = "C";
+     Persona per = new Persona();
+    }
+    class Persona{
+        
+    }
+    
+}
---
+++ EVA_2_STACK.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_stack;
+
+/**
+ *
+ * @author burci
+ */
+public class EVA_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A();
+       
+        
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        int i = 5;
+        double b = 10.5;
+    }
+    
+}
