3b3505aa306bbb499a4b7d9f63884c869b2d6f13
Alexisdbe <x55rh2f4mc@privaterelay.appleid.com>
2026-02-23 17:11:44 -0600

    Add files via upload

--- eva_1_19/src/eva_1_19/Eva_1_19.java
+++ eva_1_19/src/eva_1_19/Eva_1_19.java
@@ -1,31 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package eva_1_19;
 
 /**
- *
  * @author burci
  */
 public class Eva_1_19 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Maximo comun divisor de 180 y 48 es " + gcd(180,48));
+        System.out.println("Calculando GCD de 180 y 48:");
+        int resultado = gcd(180, 48);
+        System.out.println("-----------------------------------");
+        System.out.println("El Maximo comun divisor es: " + resultado);
     }
-    public static int gcd(int val1, int val2){
-            while (val2 != 0) {
-        int residuo = val1 % val2;
-        val1 = val2;
-        val2 = residuo;
-    }
-    return val1;
-}
-        
-    }
-    
 
+    public static int gcd(int val1, int val2) {
+        int paso = 1;
+        while (val2 != 0) {
+            int residuo = val1 % val2;
+            
+            // Imprimimos el residuo de esta iteración
+            System.out.println("Paso " + paso + ": " + val1 + " % " + val2 + " = Residuo: " + residuo);
+            
+            val1 = val2;
+            val2 = residuo;
+            paso++;
+        }
+        return val1;
+    }
+}
